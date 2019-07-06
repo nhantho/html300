f50c4d4793183ef4d0ea87b27fbb493306740aa9
nhantho <natalie.crippen@gmail.com>
2019-07-06 11:33:42 -0700

    added lesson 1 assignments

--- lesson01/assignments/scripts.js
+++ lesson01/assignments/scripts.js
@@ -1,26 +1,54 @@
-// jQuery Doc Ready
-$(function () {
-    // Set up some data and variables
-    const data = [1, 2, 3, 4],
-        input = $('.input'),
-        submit = $('.submit'),
-        resultSum = $('.sum');
-    // Click event on the submit input
-    submit.on('click', function () {
-        // Grab the input value and parse it into a number
-        const val = input.val();
-        const num = parseInt(val);
-        // Verify the user gave us a real number
-        if (!isNaN(num)) {
-            // Add our new number to the data array
-            data.push(num);
-            // Use reduce to convert our array of numbers into a single value - the sum of all of the numbers
-            const sum = data.reduce((acc, curr) => acc + curr);
-            // Add our new total to the page
-            resultSum.text(sum);
-            // Reset the input value to an empty string
-            input.val('');
-        }
-    });
-});
 
+// Establish the array of numbers to use in the buttons
+const numberList = [15, 5, 10, 4, 21, 17, 14];
+
+// The 'pretty' list of numbers to display at the top (otherwise it shows all the numbers squished together with no spaces)
+const originalNumbers = [15 + ", " + 5 + ", " + 10 + ", " + 4 + ", " + 21 + ", " + 17 + ", " + 14];
+
+$(function () {
+// Show the list of numbers in the array on the page
+  $('.originalNumbers').append(originalNumbers);
+
+//  List of the variables used
+const input1 = $('.input1'), one = $('.submit1'), resultOne = $('.filter');
+const input2 = $('.input2'), two = $('.submit2'), resultTwo = $('.find');
+const input3 = $('.input3'), three = $('.submit3'), resultThree = $('.map');
+const input4 = $('.input4'), four = $('.submit4'), resultSome = $('.some');
+
+	one.on('click', function () {
+    // Convert string into a number
+    const input = parseInt(input1.val()); {
+		// Displays all the numbers in the array that is greater than or equal to the submited value
+		const result = numberList.filter(i => i >= input);
+		resultOne.text(result);
+      }
+    });
+
+  two.on('click', function () {
+    // Convert string into a number
+    const input = parseInt(input2.val()); {
+  	// Displays the first number in the array that is greater than the submitted number
+  	const result = numberList.find(i => i > input);
+    resultTwo.text(result);
+      }
+    });
+
+  three.on('click', function () {
+     // Convert string into a number
+    const input = parseInt(input3.val()); {
+    // Adds the submited value to each number in the array
+    const result = numberList.map(i => i + input);
+    resultThree.text(result);
+      }
+    });
+
+	four.on('click', function () {
+    // Convert string into a number
+    const input = parseInt(input4.val()); {
+		// Will return "true" if submitted number is smaller than at least one number in the array
+		const result = numberList.some(i => i > input);
+		resultSome.text(result);
+      }
+    });
+
+});
